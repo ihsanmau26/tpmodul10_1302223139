4abbf3d18794eb63b970ca3974d4fae3d2cf1d1e
Ihsan Maulana <ikhsanmaulana000002@gmail.com>
2024-04-29 00:12:28 +0700

    menambahkan API mahasiswa

---
+++ tpmodul10_1302223139/Controllers/MahasiswaController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tpmodul10_1302223139.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> listMahasiswa = new List<Mahasiswa>
+    {
+        new Mahasiswa(" Ihsan Maulana", "1302223139"),
+        new Mahasiswa(" Fachruddin Ghalibi", "1302223107"),
+        new Mahasiswa(" Muhammad Fadlan Kamal", "1302223095")
+    };
+        [HttpGet]
+
+
+
+
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return listMahasiswa;
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return listMahasiswa[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa mahasiswa)
+        {
+            listMahasiswa.Add(mahasiswa);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            listMahasiswa.RemoveAt(id);
+        }
+    }
+}
